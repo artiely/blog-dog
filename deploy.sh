2c01f1bb4b75db6799aaf51a0e020a0a6e1ee94a
artiely <1119696785@qq.com>
2022-02-25 19:10:09 +0800

    [A]更新部署注释

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,11 @@ git commit -m 'deploy'
 
 
 
-# github pages
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>  记得修改docs/vuepress/config下的base为/<REPO>/
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # gitee
 # git push -f git@gitee.com:<USERNAME>/<USERNAME>.git master
