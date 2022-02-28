5e7db204a6d3b108086e311e0e1e7b2993be634e
artiely <1119696785@qq.com>
2022-02-28 09:49:19 +0800

    [A]remove theme

--- theme/lib/clientAppEnhance.js
+++
@@ -1,8 +0,0 @@
-import { defineClientAppEnhance } from '@vuepress/client'
-// import Timeline from './layouts/Timeline.vue'
-import DIframe from './components/global/iframe/DIframe.vue'
-export default defineClientAppEnhance(({ app, router, siteData }) => {
-  // ...
-  // app.component('Timeline', Timeline)
-  app.component('DIframe', DIframe)
-})
--- theme/lib/clientAppSetup.js
+++
@@ -1,7 +0,0 @@
-import { defineClientAppSetup } from '@vuepress/client';
-
-export default defineClientAppSetup(() => {
-    if (__VUEPRESS_SSR__)
-        return;
-   
-});
